6c057b0ac2b1a06e5ea5a57234a66a856d5a3b23
Uzzal-Bhowmik <ubcreativity111@gmail.com>
2024-09-30 10:44:44 +0600

    [fix] navbar link animation bug fixed

--- src/components/shared/Navbar/Components/MobileSidebar.js
+++ src/components/shared/Navbar/Components/MobileSidebar.js
@@ -16,7 +16,7 @@ const slideUp = {
   },
   animate: {
     y: 0,
-    opacity: 1,
+    // opacity: 1,
     transition: {
       type: "spring",
       stiffness: 180,
@@ -66,47 +66,47 @@ export default function MobileSidebar({ open, setOpen }) {
     }
     setOpen(false);
   };
+
   return (
     <div
       className={cn(
         "fixed inset-0 z-[99999] h-screen w-screen bg-primary-white text-primary-black transition-all duration-500 ease-in-out",
-        open ? "visible translate-x-0" : "translate-x-[100vw]",
+        open ? "translate-x-0" : "translate-x-[100vw]",
       )}
     >
-      <motion.div
-        variants={slideUp}
-        initial="initial"
-        whileInView="animate"
-        className="flex h-full flex-col items-center justify-center gap-y-3"
-      >
-        {LINKS.map((link) => (
-          <button
-            key={link.key}
-            className="overflow-hidden text-3xl font-extrabold leading-snug"
-            onClick={() => handleBtnClick(null)}
-          >
-            <motion.span variants={slideUp} key={link.key} className="block">
-              <Link href={link.route}>{link.label}</Link>
-            </motion.span>
-          </button>
-        ))}
+      {open && (
+        <motion.div
+          variants={slideUp}
+          initial="initial"
+          whileInView="animate"
+          className="flex h-full flex-col items-center justify-center gap-y-3"
+        >
+          {LINKS.map((link) => (
+            <button
+              key={link.key}
+              className="overflow-hidden text-3xl font-extrabold leading-snug"
+              onClick={() => handleBtnClick(null)}
+            >
+              <motion.span variants={slideUp} key={link.key} className="block">
+                <Link href={link.route}>{link.label}</Link>
+              </motion.span>
+            </button>
+          ))}
 
-        <div className="overflow-hidden">
-          <motion.button
-            key="signInBtn"
-            variants={slideUp}
-            className="primary-button group flex items-center gap-x-1"
-            onClick={() => handleBtnClick("/login")}
-          >
-            Sign In <AnimatedArrow />
-          </motion.button>
-        </div>
-      </motion.div>
+          <div className="overflow-hidden">
+            <motion.button
+              key="signInBtn"
+              variants={slideUp}
+              className="primary-button group flex items-center gap-x-1"
+              onClick={() => handleBtnClick("/login")}
+            >
+              Sign In <AnimatedArrow />
+            </motion.button>
+          </div>
+        </motion.div>
+      )}
 
-      <button
-        onClick={() => setOpen(false)}
-        className="absolute right-10 top-4"
-      >
+      <button onClick={() => setOpen(false)} className="absolute right-6 top-4">
         <X size={30} />
       </button>
     </div>
